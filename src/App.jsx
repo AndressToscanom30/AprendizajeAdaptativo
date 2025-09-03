0a0c338fe2dd4c2f7952c5d6c537313c715ad5d6
AndressToscanom30 <andress.toscano@gmail.com>
2025-09-03 10:47:07 -0500

    Mejor esa estructura function Home(){

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 
-const App = () => {
+function App(){
   return( 
     <BrowserRouter>
     <Navbar />
