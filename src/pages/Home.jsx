ead21189d32d98b7854e32bfb259c21cde4f7ad1
AndressToscanom30 <andress.toscano@gmail.com>
2025-09-03 10:32:25 -0500

    Commit empezando a hacer la Navbar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Bienvenido a Aprendizaje Adaptativo</h1>
+            <p>Esta es la página principal de la aplicación.</p>
+            <p className="text-blue-500">Si ven esto en azul es porque el tailwind sí sirve, si no, pues me avisan pa arreglar</p>
+        </div>
+    )
+}
+
+export default Home;
