ed5f13cfd342016b25eeff1df8df1cf6fc7211ea
AndressToscanom30 <andress.toscano@gmail.com>
2025-09-09 09:40:48 -0500

    Probando la librería Charts.js

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,16 +1,34 @@
+import BarsChart from "../components/BarsChart";
+import LinesChart from "../components/LinesChart";
+import PiesChart from "../components/PiesChart";
+
 function Test(){
     return(
         <div>
-            <h1 className="h-10 bg-amber-200 text-center mt-20 text-white">Ejemplo de gráficos con ChartJS</h1>
-            <section className="h-100 border-2">
-                <h3>Ejemplo 1: Gráfico de líneas</h3>
-            </section>
-            <section className="h-100 border-2 mt-2">
-                <h3>Ejemplo 2: Gráfico de barras</h3>
-            </section>
-            <section className="h-100 border-2 mt-2">
-                <h3>Ejemplo 3: Gráfico circular</h3>
-            </section>
+            {/* Aquí incluiré las gráficas (un componente por cada ejemplo). */}
+            <h1 className="bg-info text-center font-monospace fw-bold lh-base">Gráficas ChartJS</h1>
+            <div>
+                <p className="m-2"><b>Ejemplo #1: </b>Gráfico de líneas básico</p>
+                <div className="bg-light mx-auto px-2 border-2 border-primary" style={{width:"450px", height:"230px"}}>
+                    <LinesChart />
+                </div>
+            </div>
+            <hr className="mt-3 mb-2"/>
+            <div>
+                <p className="m-2"><b>Ejemplo #2: </b>Gráfico de barras</p>
+                <div className="bg-light mx-auto px-2 border-2 border-primary" style={{width:"450px", height:"225px"}}>
+                    <BarsChart />
+                </div>
+            </div>
+            <hr className="mt-3 mb-2"/>
+            <div>
+                <p className="m-2"><b>Ejemplo #3: </b>Gráfico circular</p>
+                <div className="bg-light mx-auto border-2 border-primary" style={{width:"450px", height:"250px"}}>
+                    <div style={{width:"100%", height:"100%", padding:"10px 0"}}>
+                        <PiesChart />                       
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
