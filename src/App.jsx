6387ce359b554b65a031eb16c99879c69871f1cb
AndressToscanom30 <andress.toscano@gmail.com>
2025-09-09 10:32:51 -0500

    Space

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,8 @@ function App(){
         <Route path="/register" element={<Register />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<Contact/>}/>
+        
+        
         <Route path="*" element={<NotFound/>} />
       </Routes>
     </BrowserRouter>
