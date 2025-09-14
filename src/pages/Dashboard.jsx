6875d92a8caa2250172f8cef7b097af5ef539895
AndressToscanom30 <andress.toscano@gmail.com>
2025-09-14 08:36:20 -0500

    Dashboard cambio mínimo, y login agregué "¿Olvidaste tu contraseña?"

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -13,7 +13,6 @@ function Dashboard() {
                     <div className="flex items-center gap-2 mb-2">
                         <img src="/AA-logo.png" alt="Logo" className="w-8 h-8" />
                         <span className="bg-blue-500 text-white px-6 py-2 rounded-full font-semibold text-base flex items-center gap-2">
-                            <span className="inline-block"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 inline-block mr-1"><path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6l4 2" /></svg></span>
                             Análisis completado con IA
                         </span>
                     </div>
