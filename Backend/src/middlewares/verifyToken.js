9127994770d008f173541703d09db760e89e6daa
AndressToscanom30 <andress.toscano@gmail.com>
2025-10-18 09:34:37 -0500

    Subo parte de sesión extendida

---
+++ Backend/src/middlewares/verifyToken.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "Token no proporcionado" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET || "secreto", (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ error: "Token inválido o expirado" });
+    }
+
+    req.user = decoded;
+    next();
+  });
+};
